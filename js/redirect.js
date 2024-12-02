a46ff5fa13f93f030e2f98419fa0d7fb96f3ceb0
V <you@example.com>
2024-12-02 13:44:20 +0300

    fullPost started

---
+++ js/redirect.js
@@ -0,0 +1,5 @@
+const btn = document.getElementsByClassName("header__btnLogIn");
+function Redirect() {
+  console.log(1);
+  window.location.replace("http://localhost:5500/html/authorized.html");
+}
